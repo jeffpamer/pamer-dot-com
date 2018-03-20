76c2eb99e118b72879dfe18d9309b39885420a6d
Jeff Pamer <jeff@mediumdivision.com>
2018-03-20 16:15:18 -0400

    Add Google Verification Meta

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,7 +6,9 @@ import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="¯\_(ツ)_/¯" />
+    <Helmet title="¯\_(ツ)_/¯">
+      <meta name="google-site-verification" content="U-MmlJpiJtURQs_IRhxEvWcrZRTYrMLrR-95dy3PRKM" />
+    </Helmet>
     {children()}
   </div>
 );
